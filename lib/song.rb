6b8456a8f3907f77f53229eb4505017d460c1aff
hugo valois <hvgaming@yahoo.com>
2018-10-02 00:29:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
- attr_reader :genres :genre_count :artist_count :artist
-  attr_accessor :
+ attr_reader :genres :genre_count :artist_count :artist :count
+  attr_accessor :name :artist :genre
   
   
   
